ad3ad9d012f5cce0093af37f43269bffe91ba7de
Bmariahyvonne <mariahyvonnebyamugisha@gmail.com>
2022-02-23 13:01:23 +0300

    current date

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 <?php
 include 'Calendar.php';
-$calendar = new Calendar('2021-02-02');
-$calendar->add_event('Birthday', '2021-02-03', 1, 'green');
-$calendar->add_event('Doctors', '2021-02-04', 1, 'red');
-$calendar->add_event('Holiday', '2021-02-16', 7);
+$calendar = new Calendar(date("l"));
+$calendar->add_event('Birthday', '2022-02-03', 1, 'green');
+$calendar->add_event('Doctors', '2022-02-04', 1, 'red');
+$calendar->add_event('Holiday', '2022-02-16', 7);
 ?>
 <!DOCTYPE html>
 <html>
